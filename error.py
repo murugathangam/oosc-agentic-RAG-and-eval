3e6616f5a179a66153b07e1fd4798d16e83b8fb1
muruguthangam <mithiranarul007@gmail.com>
2026-08-23 09:48:53 +0530

    Remove scratch and generated files from repo

--- error.py
+++
@@ -1,5 +0,0 @@
-import requests
-with open("doc.txt", "r", encoding="utf-8") as f:
-    material = f.read()
-response = requests.post("http://localhost:8000/chunk1", json={"material": material, "chunk_size": 200, "overlap": 50})
-print(response.json())
